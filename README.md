0e28c0f235bccc524d8c11475d6e5865e88af8d8
Yousef <yousefjavaherian@gmail.com>
2023-04-24 12:45:45 +0330

    changed destination to reality

--- README.md
+++ README.md
@@ -9,5 +9,7 @@ a docker image based of [teddysun/xray](https://hub.docker.com/r/teddysun/xray)
 - `UUID`: a UUID
 - `D_HOST`: destination host name (or ip) to forward requests to
 - `D_PORT`: destination listening port
-- `D_WS_PATH`: destination ws path
 - `D_UUID`: destination uuid
+- `SNI`: destination used sni host
+- `PKEY`: destination public key
+- `SHORT_ID`: destination used short id
--- entry.sh
+++ entry.sh
@@ -4,7 +4,9 @@
 /bin/sed -i "s/\$WS_PATH/$WS_PATH/g" /etc/xray/config.json
 /bin/sed -i "s/\$D_PORT/$D_PORT/g" /etc/xray/config.json
 /bin/sed -i "s/\$D_UUID/$D_UUID/g" /etc/xray/config.json
-/bin/sed -i "s/\$D_WS_PATH/$D_WS_PATH/g" /etc/xray/config.json
+/bin/sed -i "s/\$SNI/$SNI/g" /etc/xray/config.json
+/bin/sed -i "s/\$PKEY/$PKEY/g" /etc/xray/config.json
+/bin/sed -i "s/\$SHORT_ID/$SHORT_ID/g" /etc/xray/config.json
 /bin/sed -i "s/\$D_HOST/$D_HOST/g" /etc/xray/config.json
 
 cat /etc/xray/config.json
